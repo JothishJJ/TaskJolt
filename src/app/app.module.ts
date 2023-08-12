2ee8842c456538428a1b03323f600ac3ff5859ed
Jothish JJ <jothishjj11@gmail.com>
2023-08-12 19:13:55 +0530

    Add angular fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,10 @@ import { HomePageComponent } from './home-page/home-page.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { ServiceWorkerModule } from '@angular/service-worker';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent],
@@ -19,8 +23,11 @@ import { ServiceWorkerModule } from '@angular/service-worker';
       enabled: !isDevMode(),
       // Register the ServiceWorker as soon as the application is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
+      registrationStrategy: 'registerWhenStable:30000',
     }),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'taskjolt',
+    appId: '1:1013988640353:web:7bfd3eaf2adcd223978c20',
+    storageBucket: 'taskjolt.appspot.com',
+    apiKey: 'AIzaSyBM8zG4zHMZnG_g-lRUPXZdPa-7i_V0hLc',
+    authDomain: 'taskjolt.firebaseapp.com',
+    messagingSenderId: '1013988640353',
+    measurementId: 'G-07J434E14W',
+  },
+};
