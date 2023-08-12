69469851671c960bda440bb2eda76bbe2a0c5743
Jothish JJ <jothishjj11@gmail.com>
2023-08-12 14:55:38 +0530

    add fields for login page

--- src/app/shared/input/input.directive.ts
+++ src/app/shared/input/input.directive.ts
@@ -6,7 +6,7 @@ import { Directive, HostBinding } from '@angular/core';
 export class InputDirective {
   @HostBinding('class')
   classes =
-    'p-2 focus:outline-none rounded-lg focus:border-b-2 border-blue-600 bg-gray-700 peer w-full border-b placeholder:text-transparent';
+    'p-2 pt-6 focus:outline-none rounded-lg focus:border-b-2 border-blue-600 bg-gray-700 peer w-full border-b placeholder:text-transparent';
 
   constructor() {}
 }
--- src/app/shared/label/label.directive.ts
+++ src/app/shared/label/label.directive.ts
@@ -5,10 +5,10 @@ import { Directive, HostBinding } from '@angular/core';
 })
 export class LabelDirective {
   @HostBinding('class')
-  classes = `absolute left-0 ml-1 px-1 text-sm -translate-y-4 duration-100 
-  ease-linear peer-placeholder-shown:translate-y-2 peer-placeholder-shown:text-base 
-  peer-placeholder-shown:text-gray-400 peer-focus:ml-1 peer-focus:-translate-y-6 
-  peer-focus:px-1 peer-focus:text-sm`;
+  classes = `absolute left-0 ml-1 px-1 text-sm -translate-y-3 duration-100 
+  ease-linear peer-placeholder-shown:translate-y-4 peer-placeholder-shown:text-base 
+  peer-placeholder-shown:text-gray-400 peer-focus:ml-1 peer-focus:translate-y-0 
+  peer-focus:px-1 peer-focus:text-sm pointer-events-none`;
 
   constructor() {}
 }
