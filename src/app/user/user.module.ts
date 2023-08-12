06832fb71f45e806b17071f589b17a21bb1e791a
Jothish JJ <jothishjj11@gmail.com>
2023-08-12 17:21:57 +0530

    add google icon

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -5,9 +5,16 @@ import { UserRoutingModule } from './user-routing.module';
 import { LoginPageComponent } from './login-page/login-page.component';
 import { SharedModule } from '../shared/shared.module';
 import { FormsModule } from '@angular/forms';
+import { NgOptimizedImage } from '@angular/common';
 
 @NgModule({
   declarations: [LoginPageComponent],
-  imports: [CommonModule, UserRoutingModule, SharedModule, FormsModule],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    SharedModule,
+    FormsModule,
+    NgOptimizedImage,
+  ],
 })
 export class UserModule {}
