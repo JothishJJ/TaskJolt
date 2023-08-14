4c9f1e5ea7202d4c1975cda2e10f56955873c809
Jothish JJ <jothishjj11@gmail.com>
2023-08-14 14:09:25 +0530

    Change board style

--- src/app/shared/label/label.directive.ts
+++ src/app/shared/label/label.directive.ts
@@ -5,7 +5,7 @@ import { Directive, HostBinding } from '@angular/core';
 })
 export class LabelDirective {
   @HostBinding('class')
-  classes = `absolute left-0 ml-1 px-1 text-sm -translate-y-3 duration-100 
+  classes = `absolute left-0 ml-1 px-1 text-sm translate-y-0 duration-100 
   ease-linear peer-placeholder-shown:translate-y-4 peer-placeholder-shown:text-base 
   peer-placeholder-shown:text-gray-400 peer-focus:ml-1 peer-focus:translate-y-0 
   peer-focus:px-1 peer-focus:text-sm pointer-events-none`;
